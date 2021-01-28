2f7c5816cd3db6b5addb43a7b4be7870bb083daa
Erik Petrov <koteman123@hotmail.com>
2021-01-28 22:32:29 +0200

    Добавил удаление, поправил некоторые детали, добавил возможность импортировать собственные файлы.

--- Dictionary/Dictionary.py
+++ Dictionary/Dictionary.py
@@ -2,28 +2,52 @@ from tkinter import *
 import os
 
 thisFolder = os.path.dirname(os.path.abspath(__file__))
+global dec
+start = False
 
-#def fileread(file):
-#    mylist = []
-#    with open(os.path.join(thisFolder, file),encoding='utf-8') as f:
-#        lines = f.readlines()
-#        for i in lines:
-#            temp = i.split(',')
-#            mylist.append(temp[0])
-#    return mylist
+while start is not True:
+    try:
+        dec = input('Введите имя файла: ')
+        f = open(os.path.join(thisFolder,dec),'r')
+        f.close()
+        start = True
+    except FileNotFoundError:
+        print('Файл не найден. Попробуйте снова.')
 
 def delTranslation(event):
     selection = event.widget.curselection()
-    selected = toList[selection[0]]
-    selected = selected.rstrip('\n')
-    
-    pass
+    fromSelected = fromList[selection[0]].rstrip('\n')
+    toSelected = toList[selection[0]].strip("\n")
+    selected2 = f'{fromSelected},{toSelected}'
+    translation.configure(text=f'Удалено слово: {fromList[selection[0]]}')
+    with open(os.path.join(thisFolder, dec),'r',encoding='utf-8') as f:
+        lines = f.readlines()
+    with open(os.path.join(thisFolder, dec),'w',encoding='utf-8') as f:
+        for i in lines:
+            if selected2 == i.rstrip('\n'):
+                lines.remove(i)
+        for i in lines:
+            if lines.index(i) == len(lines)-1:
+                i = i.strip('\n')
+                f.write(i)
+            else:
+                f.write(i)
+    refresh()
 
 def addTranslation():
+    x=0
     word = newTrans.get()
     newTrans.delete(0,END)
-    with open(os.path.join(thisFolder,'rus-eng.txt'),'a',encoding='utf-8') as f:
-        f.write(f'\n{word}')
+    if ',' in word:
+        for i in word:
+            if i == ',':
+                x+=1
+    if x == 1:
+        with open(os.path.join(thisFolder, dec),'a',encoding='utf-8') as f:
+            f.write(f'\n{word}')
+            refresh()
+    else:
+        translation.configure(text='Неверно введенное слово, попробуйте ещё раз.')
 
 def translate(event):
     selection = event.widget.curselection()
@@ -32,10 +56,10 @@ def translate(event):
     #selected = event.widget.get(selection)
     translation.configure(text=f'Перевод этого слова: {selected}')
 
-def dump(file):
+def dump():
     global fromList,toList
     fromList,toList = [],[]
-    with open(os.path.join(thisFolder, file),encoding='utf-8')as f:
+    with open(os.path.join(thisFolder, dec),encoding='utf-8')as f:
         lines = f.readlines()
         for i in lines:
             fromI = i.split(',')
@@ -47,11 +71,11 @@ def refresh():
     if listbox:
         listbox.delete(0,END)
 #    fromList,toList = dump('rus-eng.txt')
-    dump('rus-eng.txt')
+    dump()
     for i in fromList:
         listbox.insert(END,i)
 
-#TODO: сделать кнопку для удаления перевода, навести порядок в программе, додумать ещё функции.
+#TODO: навести порядок в программе, додумать ещё функции.
 
 main = Tk()
 mainMenu = Menu(main)
@@ -77,10 +101,13 @@ refreshbutton = Button(main,
 w.create_window(50,50,window=refreshbutton)
 
 newTrans = Entry(main)
-w.create_window(350,50,window=newTrans)
+w.create_window(350,150,window=newTrans)
 
 transBut = Button(main,command=addTranslation,text='Добавить слово')
-w.create_window(230,50,window=transBut)
+w.create_window(230,150,window=transBut)
+
+guide = Label(main,text='При добавлении перевода пишите его в след формате:\n оригинал,перевод')
+w.create_window(230,120,window=guide)
 
 refresh()
 
@@ -91,7 +118,8 @@ refresh()
 
 #отказался от лямбды, сделал списки глобальными
 #не знаю насколько это правильно, но оно работает
-newTrans.bind('<<Return>>',addTranslation)
-listbox.bind('<<3>>',delTranslation)
+#newTrans.bind('<Return>',addTranslation)
+listbox.bind('<Double-1>',delTranslation)
+#кнопки должны быть в <> но не в <<>>
 listbox.bind('<<ListboxSelect>>',translate)
 main.mainloop()
